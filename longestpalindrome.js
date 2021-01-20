18d279a5af17cad2d7220b81b95da2d8463b684e
Patkloe <52906039+Patkloe@users.noreply.github.com>
2021-01-20 09:21:59 -0600

    Update longestpalindrome.js

--- longestpalindrome.js
+++ longestpalindrome.js
@@ -8,18 +8,29 @@ function is_palindrome(t){
     return true;
   return false;
 }
-function longestpalindrome(s){
- var ins = {};
+var longestPalindrome = function(s) {
+  var ins = {};
  var ind = 0;
  var max = 0;
  var res = "";
+if(s.length === 0 || s.length === 1){
+   return s;
+   }
+if(s.length === 2 && s[0] != s[1]){
+   return s[0];
+   }
+if(is_palindrome(s)){
+    return s;
+}
+ else{
  for(var i = 0; i < s.length; i++){
      let val = s[i];
+      //ins[val] = i;
      if(val in ins){
        ind = ins[val];
        console.log(" index in dict :" + " " + ind + " " + "in table :" + " " + i);
        let pal = "";
-       while(ind <= i){
+       while(ind  <= i){
            //alert("start");
          pal = pal + s[ind];
         ind++;
@@ -34,8 +45,9 @@ function longestpalindrome(s){
        }
      }
      ins[val] = i;
- }
+ } // end for
  return res;
-}
+ }
+};
  var test = "abracaacbra";
      longestpalindrome(test);
